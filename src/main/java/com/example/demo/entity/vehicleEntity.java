e66bcdd7656041d0567c07fee50afb52c64bca99
bala-amypo <bala@amypo.in>
2025-12-16 16:45:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/vehicleEntity.java
+++ src/main/java/com/example/demo/entity/vehicleEntity.java
@@ -3,3 +3,23 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 
 @Entity
+@Table(
+    name="vehicle",
+    uniqueConstraints={
+        @uniqueConstraints(columnsNames="vehicle_number")
+    }
+)
+public class Vehicle{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id",nullable=false)
+    private User user;
+
+    @Column(name="vehicle_number",nullable=false)
+    private String vehicleNumber;
+
+    @Column(name="cpacity_kg",)
+}
