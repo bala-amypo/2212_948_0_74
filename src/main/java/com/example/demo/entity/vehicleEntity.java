bd21b89d286df02c0d77d7beb5a4074fd391aba9
bala-amypo <bala@amypo.in>
2025-12-16 16:50:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/vehicleEntity.java
+++ src/main/java/com/example/demo/entity/vehicleEntity.java
@@ -21,5 +21,29 @@ public class Vehicle{
     @Column(name="vehicle_number",nullable=false)
     private String vehicleNumber;
 
-    @Column(name="cpacity_kg",)
+    @Column(name="cpacity_kg",nullable=false)
+    private Double capacityKg;
+
+    @Column(name="fuel_efficiency")
+    private Double fuelEfficiency;
+
+    public Vehicle(){}
+
+    public Vehicle(User user,String vehicleNumber,Double capacityKg,Double fuelEfficiency){
+        this.user=user;
+        this.vehicleNumber=vehicleNumber;
+        setCapacityKg(capacityKg);
+        this.fuelEfficiency=fuelEfficiency;
+    }
+    public Long gatId(){
+        return id;
+    }
+
+    public User getUser(){
+        return user;
+    }
+    public void setUser(User user){
+        this.user=user;
+    }
+    public String getVehicleN
 }
